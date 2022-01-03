89e4a5dd27f0b70bca0fe72e12075e651b926486
P.O Marec <po@marec.me>
2022-01-03 09:39:58 +0100

    test : pg relationship & operators

--- test/postgres_test.dart
+++ test/postgres_test.dart
@@ -3,6 +3,8 @@
 import 'package:postgres/postgres.dart';
 import 'package:test/test.dart';
 
+import '../bin/nodes/operators/join_one_to_one.dart';
+import '../bin/nodes/operators/operator_node.dart';
 import '../bin/nodes/sources/postgres_table.dart';
 
 void main() async {
@@ -168,6 +170,75 @@ void main() async {
             'users': {'id': 1, 'name': 'omar'}
           },
         ],
+      ]),
+    );
+  });
+
+  test('Postgres table with relationship & operators', () async {
+    // Setup db
+    final sql = """
+      DROP TABLE IF EXISTS "users";
+      CREATE TABLE "public"."users" (
+          "id" integer NOT NULL,
+          "name" character varying NOT NULL,
+          "parent" integer NOT NULL
+      ) WITH (oids = false);
+
+      ALTER TABLE "users" REPLICA IDENTITY FULL;
+
+      INSERT INTO "users" ("id", "name", "parent") VALUES
+      (0,	'fred', 1),
+      (1,	'omar', 0),
+      (2,	'pataf', 0),
+      (3,	'skavinski', 0);
+    """;
+    await connection.execute(sql);
+
+    // Setup nodes
+
+    final usersDBSource = await PostgresTableSource(connection, 'users');
+
+    final usersWithParent = await JoinOneToOne(
+      usersDBSource,
+      'parent',
+      usersDBSource,
+      'id',
+    );
+    final usersWithChildrenCount = await NodeOperator1Input(
+      (a) async => a,
+      usersWithParent,
+    );
+
+    // Test
+    expect(
+      usersWithChildrenCount.stream,
+      emitsInOrder([
+        [
+          {
+            'users': {'id': 0, 'name': "fred", 'parent': 1},
+            'parent': {
+              'users': {'id': 0, 'name': "fred", 'parent': 1}
+            }
+          },
+          {
+            'users': {'id': 1, 'name': "omar", 'parent': 0},
+            'parent': {
+              'users': {'id': 0, 'name': "fred", 'parent': 1}
+            }
+          },
+          {
+            'users': {'id': 2, 'name': "pataf", 'parent': 0},
+            'parent': {
+              'users': {'id': 0, 'name': "fred", 'parent': 1}
+            }
+          },
+          {
+            'users': {'id': 3, 'name': "skavinski", 'parent': 0},
+            'parent': {
+              'users': {'id': 0, 'name': "fred", 'parent': 1}
+            }
+          }
+        ]
       ]),
     );
   });
